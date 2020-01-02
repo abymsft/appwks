12bb6606ddf3891b3ea0f3e07e72e7867e6ed087
Mads Damgård <madsd@microsoft.com>
2020-01-02 16:36:37 +0100

    Fix json serialization

--- src/client/Startup.cs
+++ src/client/Startup.cs
@@ -26,6 +26,7 @@ namespace client
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddHttpClient();
             services.AddRazorPages();
             services.AddServerSideBlazor();
             services.AddSingleton<WeatherForecastService>();
